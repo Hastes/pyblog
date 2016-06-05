3c97a695c64dc901850853bd350f384d4f5160c4
Hastes <rom541997@gmail.com>
2016-06-05 19:46:32 +0800

    dsadaw

--- pyt1/wsgi.py
+++ pyt1/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyt1.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
