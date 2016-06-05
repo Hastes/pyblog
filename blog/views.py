df1ff1e8da1db120fad08a91ca925274b83558ea
Hastes <rom541997@gmail.com>
2016-06-05 17:57:03 +0800

    greee

--- blog/views.py
+++ blog/views.py
@@ -30,15 +30,3 @@ def post_new(request):
             form = PostForm()
         return render(request, 'post_edit.html', {'form': form})
 
-def PostHabr(request):
-    post = Post.objects.filter(published_date__lte=timezone.now())[0]
-    oldtitle = post.title
-    newtitle= GetTitleHabr()
-    print(oldtitle, newtitle)
-    if oldtitle != newtitle:
-        title = GetTitleHabr()
-        oldtitle=title
-        Post.objects.create(author=request.user, title=title, text=GetContentHabr(),published_date = timezone.now())
-        return HttpResponseRedirect("http://localhost:8000/")
-    else:
-        return HttpResponse("Post usje dobavlen")
