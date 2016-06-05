1b9fa25f2fa866068c769f0265d3177ff3322e12
Hastes <rom541997@gmail.com>
2016-06-05 20:05:16 +0800

    dsadad

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()[:5]
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'index2.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
@@ -30,3 +30,15 @@ def post_new(request):
             form = PostForm()
         return render(request, 'post_edit.html', {'form': form})
 
+###def PostHabr(request):
+  #  post = Post.objects.filter(published_date__lte=timezone.now())[0]
+   # oldtitle = post.title
+  #  newtitle= GetTitleHabr()
+  #  print(oldtitle, newtitle)
+  #  if oldtitle != newtitle:
+  #      title = GetTitleHabr()
+   #     oldtitle=title
+  #      Post.objects.create(author=request.user, title=title, text=GetContentHabr(),published_date = timezone.now())
+   #     return HttpResponseRedirect("http://localhost:8000/")
+  #  else:
+ #       return HttpResponse("Post usje dobavlen")
