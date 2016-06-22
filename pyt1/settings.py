f2aba9b1abffed0d011037e449e015f01e54b33c
Hastes <rom541997@gmail.com>
2016-06-22 20:54:15 +0700

    untracked trackk

--- pyt1/settings.py
+++ pyt1/settings.py
@@ -30,17 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = {
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loginapp',
     'blog',
-    'loginapp'
-}
-
+]
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
